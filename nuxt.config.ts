f1c9219a6078514db36e431e5aedcc5e01c4270d
Nick Rabone <n.rabone72@aol.com>
2024-06-30 20:36:35 +0100

    Brought in Tailwind and Flowbite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css'
+        }
+      ],
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js'
+        }
+      ]
+    }
+  }
 })
