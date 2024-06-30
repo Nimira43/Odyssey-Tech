5a07d468cb9ba34e069f3ba98ad4e9131c3b3bb4
Nick Rabone <n.rabone72@aol.com>
2024-06-30 21:56:57 +0100

    Created MovieCard component

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -1,9 +1,40 @@
 <template>
     <div>
-
+        <div class="max-w-sm p-6 bg-[#eee] border border-[#ccc] rounded-sm shadow dark:bg-[#111] dark:border-[#333]">
+            <NuxtLink @click="changeItemState" :to="`/item/${title}`">
+                <img class="rounded-t-sm" :src="`${imageSiteUrl}${poster_path}`" alt="Movie">
+            </NuxtLink>
+            <div class="p-5">
+                <span href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-[#111] dark:text-[#eee]">{{ title }}</h5>
+                </span>
+                <p class="mb-3 font-normal text-[#111] dark:text-[#eee]">Release Date: {{ release_date }}</p>
+                <p v-if="overview" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#eee] bg-[#ff4500] rounded-sm hover:bg-[#ff7c00] focus:ring-4 focus:outline-none focus:ring-[#eef700] dark:bg-[#ff5500] dark:hover:bg-[#ff4500] dark:focus:ring-[#ff7c00]">
+                    {{ overview }}
+                </p>
+                <p v-else class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#eee] bg-[#ff4500] rounded-sm hover:bg-[#ff7c00] focus:ring-4 focus:outline-none focus:ring-[#eef700] dark:bg-[#ff5500] dark:hover:bg-[#ff4500] dark:focus:ring-[#ff7c00]">
+                    No Overview Available
+                </p>
+                
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
+    defineProps([
+        'title'.
+        'id',
+        'overview',
+        'release_date',
+        'popularity',
+        'poster_path',
+        'item'
+    ])
 
+    const imageSiteUrl = 'https://image.tmdb.org/t/p/w500/'
+    const itemState = useState('chosenItem', () => {})
+    const changeItemState = (item) => {
+        itemState.value = item
+    }
 </script>
