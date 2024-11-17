13a785f74a20c0dac292b296cd6deb7409e3faee
Nimira43 <n.rabone72@aol.com>
2024-11-17 11:23:59 +0000

    Developing the MovieCard

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -5,5 +5,16 @@
 </template>
 
 <script setup>
+  defineProps([
+    'title',
+    'id',
+    'overview',
+    'release_date',
+    'popularity',
+    'poster_path',
+    'item'
+  ])
+
+  const imageSiteUrl = 'https://image.tmdb.org/t/p/w500/'
 
 </script>
