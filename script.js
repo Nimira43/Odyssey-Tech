8707e4f2547d29a6c90131a37d5061b12945d9c6
Nimira43 <n.rabone72@aol.com>
2025-03-09 10:26:57 +0000

    Styled lines and uncommented toggle JS. Menu Icon transforms on click.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// const menuIcon = document.querySelector('.menu-icon')
-// const container = document.querySelector('.container')
+const menuIcon = document.querySelector('.menu-icon')
+const container = document.querySelector('.container')
 
-// menuIcon.addEventListener('click', () => container.classList.toggle('change'))
+menuIcon.addEventListener('click', () => container.classList.toggle('change'))
 
-console.log('Connected!!!')
